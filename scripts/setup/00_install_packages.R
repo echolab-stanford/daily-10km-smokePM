50e83a739649a28337b05b16c3b3fc1a50889cbd
jb-li <79426034+jb-li@users.noreply.github.com>
2022-08-01 16:46:12 -0700

    Initial list of packages to load

---
+++ scripts/setup/00_install_packages.R
@@ -0,0 +1,1 @@
+# devtools::install_github("zeehio/facetscales")
---
+++ scripts/setup/00_load_settings.R
@@ -0,0 +1,4 @@
+#-------------------------------------------------------------------------------
+# Provide your US Census API Key
+key <- "INSERT YOUR US CENSUS BUREAU API KEY HERE"
+census_api_key(key)
