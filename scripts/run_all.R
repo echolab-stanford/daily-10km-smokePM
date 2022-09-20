441dfe54159dca797d61f2f10273b3e8395fd088
jb-li <79426034+jb-li@users.noreply.github.com>
2022-09-20 10:59:57 -0700

    Incorporate grid making scripts

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -1,3 +1,3 @@
-source("scripts/setup/00_load_packages.R")
-source("scripts/setup/00_load_functions.R")
-source("scripts/setup/00_load_settings.R")
+source("scripts/setup/00_01_load_packages.R")
+source("scripts/setup/00_02_load_functions.R")
+source("scripts/setup/00_03_load_settings.R")
