3768ddfeb4e070de611a5980d029f382ca91236f
jb-li <79426034+jb-li@users.noreply.github.com>
2022-09-21 16:32:19 -0700

    Source setup scripts in each main script

--- scripts/main/01_02_predict_AOD_Sherlock.R
+++ scripts/main/01_02_predict_AOD_Sherlock.R
@@ -1,3 +1,7 @@
+source("scripts/setup/00_01_load_packages.R")
+source("scripts/setup/00_02_load_functions.R")
+source("scripts/setup/00_03_load_settings.R")
+
 num_cores = as.integer(Sys.getenv("SLURM_CPUS_PER_TASK")) - 1
 
 #-------------------------------------------------------------------------------
--- scripts/main/03_02_figure01.R
+++ scripts/main/03_02_figure01.R
@@ -1,3 +1,7 @@
+source("scripts/setup/00_01_load_packages.R")
+source("scripts/setup/00_02_load_functions.R")
+source("scripts/setup/00_03_load_settings.R")
+
 # ------------------------------------------------------------------------------
 # Written by: Marissa Childs
 # Plots schematic.
